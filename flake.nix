2492a686e6e0334d8870a672587a26587c0a253e
aster <137767097+aster-void@users.noreply.github.com>
2025-04-30 10:52:04 +0900

    init shell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import rust-overlay)
+        ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+        ];
+      };
+    });
+}
